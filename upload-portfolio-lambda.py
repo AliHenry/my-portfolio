17e6dde54650fc92c4932d0ca0f7bb0a2ac44d8b
AliHenry <aliyuhgarba@gmail.com>
2019-06-20 16:57:56 +0700

    python function fix 2

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -2,20 +2,23 @@ import boto3
 from botocore.client import Config
 import StringIO
 import zipfile
-import mimetypes
 
-s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+def lambda_handler(event, context):
+    s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
 
-portfolio_bucket = s3.Bucket('portfolio.aliyuhenrygarba')
-build_bucket = s3.Bucket('portfoliobuild.aliyuhenrygarba.info')
+    portfolio_bucket = s3.Bucket('portfolio.aliyuhenrygarba')
+    build_bucket = s3.Bucket('portfoliobuild.aliyuhenrygarba.info')
 
-portfolio_zip = StringIO.StringIO()
-build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+    portfolio_zip = StringIO.StringIO()
+    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
 
-with zipfile.ZipFile(portfolio_zip) as myzip:
-    for nm in myzip.namelist():
-        obj = myzip.open(nm)
-        portfolio_bucket.upload_fileobj(obj, nm, 
-            ExtraArgs={'ContentType': 'basestring'})
-        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+    with zipfile.ZipFile(portfolio_zip) as myzip:
+        for nm in myzip.namelist():
+            obj = myzip.open(nm)
+            portfolio_bucket.upload_fileobj(obj, nm)
+            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+
+    print('Job done!')
+
+    return 'Hello from lambda'
 
